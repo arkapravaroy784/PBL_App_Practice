51133e393efdb6948f2fdbf6527e38b3656dc359
Arkaprava Roy <arkapravaroy784@gmail.com>
2026-07-25 21:45:30 +0530

    Add comment explaining command-line argument usage
    Added a comment to describe the program's functionality.

--- JAVA/Java Fundamentals/ass1.java
+++ JAVA/Java Fundamentals/ass1.java
@@ -1,3 +1,4 @@
+//Write a Java program that accepts two Strings as command-line arguments and generates the output in the required format.
 class ass1 {
     public static void main(String[] args) {
 
