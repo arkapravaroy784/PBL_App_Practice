8001c588e10fcd8cfcf51f7cc55512b03de4cd5c
Arkaprava Roy <arkapravaroy784@gmail.com>
2026-07-26 16:45:18 +0530

    Add files via upload

---
+++ JAVA/Java Fundamentals/ass5.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+class ass5 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        int num1 = sc.nextInt();
+
+        System.out.print("Enter second number: ");
+        int num2 = sc.nextInt();
+
+        if ((num1 % 10) == (num2 % 10)) {
+            System.out.println("true");
+        } else {
+            System.out.println("false");
+        }
+
+        sc.close();
+    }
+}
