209b5f632362ab63175213d8c4b6842ec67291b4
Arkaprava Roy <arkapravaroy784@gmail.com>
2026-07-26 16:42:24 +0530

    Add files via upload

---
+++ JAVA/Java Fundamentals/ass3.java
@@ -0,0 +1,16 @@
+class ass3 {
+    public static void main(String[] args) {
+
+        if (args.length != 2) {
+            System.out.println("Please pass two integer command line arguments.");
+            return;
+        }
+
+        int num1 = Integer.parseInt(args[0]);
+        int num2 = Integer.parseInt(args[1]);
+
+        int sum = num1 + num2;
+
+        System.out.println("The sum of " + num1 + " and " + num2 + " is " + sum);
+    }
+}
