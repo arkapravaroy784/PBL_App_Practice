fdbf7cf17ec81fb4cc8ba86c37e671224891a4a6
Arkaprava Roy <arkapravaroy784@gmail.com>
2026-07-27 22:00:44 +0530

    Add files via upload

---
+++ JAVA/Java Fundamentals/ass10.java
@@ -0,0 +1,28 @@
+class ass10 {
+    public static void main(String[] args) {
+
+        if (args.length != 2) {
+            System.out.println("Please pass Gender and Age as command line arguments.");
+            return;
+        }
+
+        String gender = args[0];
+        int age = Integer.parseInt(args[1]);
+
+        if (gender.equalsIgnoreCase("Female")) {
+            if (age >= 1 && age <= 58) {
+                System.out.println("Percentage of Interest = 8.2%");
+            } else if (age >= 59 && age <= 100) {
+                System.out.println("Percentage of Interest = 9.2%");
+            }
+        } else if (gender.equalsIgnoreCase("Male")) {
+            if (age >= 1 && age <= 58) {
+                System.out.println("Percentage of Interest = 8.4%");
+            } else if (age >= 59 && age <= 100) {
+                System.out.println("Percentage of Interest = 10.5%");
+            }
+        } else {
+            System.out.println("Invalid Gender");
+        }
+    }
+}
