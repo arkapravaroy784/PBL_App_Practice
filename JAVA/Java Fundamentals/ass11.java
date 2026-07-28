296ca0de28a61d114084978235e58093bdaee17d
Arkaprava Roy <arkapravaroy784@gmail.com>
2026-07-28 13:18:38 +0530

    Fix formatting and maintain functionality in ass11.java

--- JAVA/Java Fundamentals/ass11.java
+++ JAVA/Java Fundamentals/ass11.java
@@ -1,6 +1,7 @@
 // Initialize a character variable with an alphabet in a Java program.
 //• If the character is in lowercase, convert it to uppercase and display it in the format: a->A
 //• If the character is in uppercase, convert it to lowercase and display it in the format: A->a
+
 class ass11 {
     public static void main(String[] args) {
 
