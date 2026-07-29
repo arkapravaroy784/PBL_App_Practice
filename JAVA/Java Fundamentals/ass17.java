d1f45f6bbfae4b9e728577c57381f36a824fed4f
Arkaprava Roy <arkapravaroy784@gmail.com>
2026-07-29 10:41:12 +0530

    Remove commented code and retain functionality

--- JAVA/Java Fundamentals/ass17.java
+++ JAVA/Java Fundamentals/ass17.java
@@ -1,4 +1,4 @@
-//Write a Java program to print all prime numbers between 10 and 99.
+//Write a Java program to print all prime numbers between 10 and 99
 
 class ass17 {
     public static void main(String[] args) {
