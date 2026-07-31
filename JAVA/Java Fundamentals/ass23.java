42859020b0a06adcecc8f5c11066b88112f0e5b4
Arkaprava Roy <arkapravaroy784@gmail.com>
2026-07-31 07:09:38 +0530

    Add files via upload

---
+++ JAVA/Java Fundamentals/ass23.java
@@ -0,0 +1,26 @@
+class ass23 {
+    public static void main(String[] args) {
+
+        // Initialize the array
+        int[] arr = {25, 10, 45, 5, 30};
+
+        // Assume first element is both maximum and minimum
+        int max = arr[0];
+        int min = arr[0];
+
+        // Find maximum and minimum
+        for (int i = 1; i < arr.length; i++) {
+
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+
+            if (arr[i] < min) {
+                min = arr[i];
+            }
+        }
+
+        System.out.println("Maximum = " + max);
+        System.out.println("Minimum = " + min);
+    }
+}
