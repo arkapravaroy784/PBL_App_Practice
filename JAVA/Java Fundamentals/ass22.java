9c502439f3b25acea77b7d95f3ff1a1009fe67d1
Arkaprava Roy <arkapravaroy784@gmail.com>
2026-07-31 07:08:14 +0530

    Add files via upload

---
+++ JAVA/Java Fundamentals/ass22.java
@@ -0,0 +1,20 @@
+class ass22 {
+    public static void main(String[] args) {
+
+        // Initialize the array
+        int[] arr = {10, 20, 30, 40, 50};
+
+        int sum = 0;
+
+        // Calculate sum
+        for (int i = 0; i < arr.length; i++) {
+            sum = sum + arr[i];
+        }
+
+        // Calculate average
+        double average = (double) sum / arr.length;
+
+        System.out.println("Sum = " + sum);
+        System.out.println("Average = " + average);
+    }
+}
