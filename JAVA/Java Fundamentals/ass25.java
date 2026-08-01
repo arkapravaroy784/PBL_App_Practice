17db67784b670f19964af546ea722b86a719c33c
Arkaprava Roy <arkapravaroy784@gmail.com>
2026-08-01 09:36:05 +0530

    Add files via upload

---
+++ JAVA/Java Fundamentals/ass25.java
@@ -0,0 +1,12 @@
+class ass25 {
+    public static void main(String[] args) {
+
+        // Initialize the array with ASCII values
+        int[] arr = {65, 66, 67, 68, 69};
+
+        // Print corresponding characters
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print((char) arr[i] + " ");
+        }
+    }
+}
