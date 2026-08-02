32cdb031024e11291c6978faeb93e2eaea9d767a
Arkaprava Roy <arkapravaroy784@gmail.com>
2026-08-02 11:33:42 +0530

    Add files via upload

---
+++ JAVA/Java Fundamentals/ass27.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+
+class ass27 {
+    public static void main(String[] args) {
+
+        // Initialize the array
+        int[] arr = {25, 10, 45, 5, 30};
+
+        // Sort the array
+        Arrays.sort(arr);
+
+        // Print the sorted array
+        System.out.println("Sorted Array:");
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
