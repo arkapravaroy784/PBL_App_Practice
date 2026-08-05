0f38c15a733d88f99ffe0272ac6c6ce533801694
Arkaprava Roy <arkapravaroy784@gmail.com>
2026-08-05 08:14:17 +0530

    Add files via upload

---
+++ JAVA/OOPs & Inheritance/TestBook.java
@@ -0,0 +1,118 @@
+class Author {
+
+    private String name;
+    private String email;
+    private char gender;
+
+    // Parameterized Constructor
+    Author(String name, String email, char gender) {
+        this.name = name;
+        this.email = email;
+        this.gender = gender;
+    }
+
+    // Getters
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public char getGender() {
+        return gender;
+    }
+
+    // Setters
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setGender(char gender) {
+        this.gender = gender;
+    }
+}
+
+class Book {
+
+    private String name;
+    private Author author;
+    private double price;
+    private int qtyInStock;
+
+    // Parameterized Constructor
+    Book(String name, Author author, double price, int qtyInStock) {
+        this.name = name;
+        this.author = author;
+        this.price = price;
+        this.qtyInStock = qtyInStock;
+    }
+
+    // Getters
+    public String getName() {
+        return name;
+    }
+
+    public Author getAuthor() {
+        return author;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public int getQtyInStock() {
+        return qtyInStock;
+    }
+
+    // Setters
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAuthor(Author author) {
+        this.author = author;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public void setQtyInStock(int qtyInStock) {
+        this.qtyInStock = qtyInStock;
+    }
+}
+
+class TestBook {
+
+    public static void main(String[] args) {
+
+        // Create Author object
+        Author author = new Author(
+                "J.K. Rowling",
+                "jkrowling@gmail.com",
+                'F');
+
+        // Create Book object
+        Book book = new Book(
+                "Harry Potter",
+                author,
+                599.50,
+                25);
+
+        // Print Book Details
+        System.out.println("Book Name : " + book.getName());
+        System.out.println("Price : " + book.getPrice());
+        System.out.println("Quantity : " + book.getQtyInStock());
+
+        // Print Author Details
+        System.out.println("Author Name : " + book.getAuthor().getName());
+        System.out.println("Author Email : " + book.getAuthor().getEmail());
+        System.out.println("Author Gender : " + book.getAuthor().getGender());
+    }
+}
