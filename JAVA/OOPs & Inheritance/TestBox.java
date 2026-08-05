03f82adb2db27b2e435a75cb463038fc9d2d086f
Arkaprava Roy <arkapravaroy784@gmail.com>
2026-08-05 08:10:24 +0530

    Update TestBox.java

--- JAVA/OOPs & Inheritance/TestBox.java
+++ JAVA/OOPs & Inheritance/TestBox.java
@@ -2,6 +2,7 @@
 //The dimensions of the box are: Width, Height, Depth
 //The class should have a method that returns the volume of the box.
 //Create an object of the Box class and test the functionalities.
+
 class Box {
 
     double width;
