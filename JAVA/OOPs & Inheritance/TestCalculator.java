ee2f4c6c1a4832212c87542295646931b576d35f
Arkaprava Roy <arkapravaroy784@gmail.com>
2026-08-05 08:13:01 +0530

    Implement Calculator class with power methods

--- JAVA/OOPs & Inheritance/TestCalculator.java
+++ JAVA/OOPs & Inheritance/TestCalculator.java
@@ -1,3 +1,10 @@
+//Create a new class called Calculator with the following methods:
+//1. A static method called powerInt(int num1, int num2).
+//o This method should return num1 raised to the power num2.
+//2. A static method called powerDouble(double num1, int num2).
+//o This method should return num1 raised to the power num2.
+//3. Invoke both the methods and test the functionalities.
+//Hint: Use Math.pow(double, double) to calculate the power.
 class Calculator {
 
     // Static method for integer values
