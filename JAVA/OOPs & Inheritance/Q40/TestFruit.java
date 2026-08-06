7940286550aecdb4fdd452c7cd7fcbafdaf485df
Arkaprava Roy <arkapravaroy784@gmail.com>
2026-08-06 11:03:05 +0530

    Add files via upload

---
+++ JAVA/OOPs & Inheritance/Q40/TestFruit.java
@@ -0,0 +1,14 @@
+class TestFruit {
+
+    public static void main(String[] args) {
+
+        Apple apple = new Apple();
+        Orange orange = new Orange();
+
+        apple.eat();
+
+        System.out.println();
+
+        orange.eat();
+    }
+}
