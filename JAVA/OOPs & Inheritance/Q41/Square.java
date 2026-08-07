7ddd4388fef331e9852a93a2d9be0f23eab6d59e
Arkaprava Roy <arkapravaroy784@gmail.com>
2026-08-07 09:01:36 +0530

    Add files via upload

---
+++ JAVA/OOPs & Inheritance/Q41/Square.java
@@ -0,0 +1,12 @@
+class Square extends Shape {
+
+    @Override
+    void draw() {
+        System.out.println("Drawing Square");
+    }
+
+    @Override
+    void erase() {
+        System.out.println("Erasing Square");
+    }
+}
