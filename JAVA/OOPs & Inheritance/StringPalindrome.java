33cab021ee626331e3a9b72962c59f156c5afd35
Arkaprava Roy <arkapravaroy784@gmail.com>
2026-08-07 09:04:02 +0530

    Add files via upload

---
+++ JAVA/OOPs & Inheritance/StringPalindrome.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+class StringPalindrome {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String str = sc.nextLine();
+
+        String reverse = "";
+
+        for (int i = str.length() - 1; i >= 0; i--) {
+            reverse = reverse + str.charAt(i);
+        }
+
+        if (str.equalsIgnoreCase(reverse)) {
+            System.out.println(str + " is a Palindrome");
+        } else {
+            System.out.println(str + " is not a Palindrome");
+        }
+
+        sc.close();
+    }
+}
