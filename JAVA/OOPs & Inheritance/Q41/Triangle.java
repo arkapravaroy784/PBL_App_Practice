32fc80c6c7e2bb819b979b1319ed9e3ea67c1970
Arkaprava Roy <arkapravaroy784@gmail.com>
2026-08-07 09:01:11 +0530

    Add files via upload

---
+++ JAVA/OOPs & Inheritance/Q41/Triangle.java
@@ -0,0 +1,12 @@
+class Triangle extends Shape {
+
+    @Override
+    void draw() {
+        System.out.println("Drawing Triangle");
+    }
+
+    @Override
+    void erase() {
+        System.out.println("Erasing Triangle");
+    }
+}
