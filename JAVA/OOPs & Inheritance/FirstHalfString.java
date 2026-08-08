5344f9c59678e9955cd90b021e83e9dcc62f05ff
Arkaprava Roy <arkapravaroy784@gmail.com>
2026-08-09 00:03:53 +0530

    Update FirstHalfString.java

--- JAVA/OOPs & Inheritance/FirstHalfString.java
+++ JAVA/OOPs & Inheritance/FirstHalfString.java
@@ -1,3 +1,4 @@
+//Write a Java program that returns the first half of the string, if the length of the string is even. It should return "null" for an odd-length string.
 import java.util.Scanner;
 
 class FirstHalfString {
