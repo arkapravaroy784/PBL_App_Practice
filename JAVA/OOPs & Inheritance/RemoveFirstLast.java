10eaa32e6c379c85571adb316220abb8e922ed25
Arkaprava Roy <arkapravaroy784@gmail.com>
2026-08-10 09:28:42 +0530

    Add files via upload

---
+++ JAVA/OOPs & Inheritance/RemoveFirstLast.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+class RemoveFirstLast {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String str = sc.nextLine();
+
+        System.out.println(str.substring(1, str.length() - 1));
+
+        sc.close();
+    }
+}
