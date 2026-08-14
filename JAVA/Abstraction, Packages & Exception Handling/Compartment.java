c534e1e675d7b755467373a8c0a6dbd70b764b6b
Arkaprava Roy <arkapravaroy784@gmail.com>
2026-08-14 13:47:31 +0530

    Update Compartment.java

--- JAVA/Abstraction, Packages & Exception Handling/Compartment.java
+++ JAVA/Abstraction, Packages & Exception Handling/Compartment.java
@@ -1,3 +1,9 @@
+//Create a class called Compartment that represents a ship compartment with the following attributes:
+//• Height
+//• Width
+//• Breadth
+//Important: This class should not conflict with the Compartment class created in the previous abstract class exercise. To avoid the conflict, create this class in a new package called:
+//com.wipro.automobile.ship
 package com.wipro.automobile.ship;
 
 public class Compartment {
