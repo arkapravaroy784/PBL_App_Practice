d7fb96fcf8fec85416563d4437155f3032a15892
Arkaprava Roy <arkapravaroy784@gmail.com>
2026-08-19 09:47:52 +0530

    Add files via upload

---
+++ JAVA/Abstraction, Packages & Exception Handling/Q68/InvalidCountryException.java
@@ -0,0 +1,11 @@
+class InvalidCountryException extends Exception {
+
+    public InvalidCountryException() {
+        super();
+    }
+
+    public InvalidCountryException(String message) {
+        super(message);
+    }
+
+}
