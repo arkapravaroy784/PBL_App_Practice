02f4b7b8ee752b18bc671b72b8a9ee6e2250e37d
Arkaprava Roy <arkapravaroy784@gmail.com>
2026-09-14 21:17:10 +0530

    Add files via upload

---
+++ JAVA/Collection/StringArrayList.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class StringArrayList {
+
+    // Method to print all elements using Iterator
+    public static void printAll(ArrayList<String> list) {
+
+        Iterator<String> iterator = list.iterator();
+
+        while (iterator.hasNext()) {
+            System.out.println(iterator.next());
+        }
+    }
+
+    public static void main(String[] args) {
+
+        ArrayList<String> list = new ArrayList<>();
+
+        // Adding Strings
+        list.add("Java");
+        list.add("Python");
+        list.add("C");
+        list.add("C++");
+        list.add("JavaScript");
+        list.add("SQL");
+
+        System.out.println("Elements of the ArrayList:");
+
+        printAll(list);
+
+    }
+}
