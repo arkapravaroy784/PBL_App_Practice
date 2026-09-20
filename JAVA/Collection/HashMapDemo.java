7848efa965ca38d77df0b425252d5abf35124947
Arkaprava Roy <arkapravaroy784@gmail.com>
2026-09-20 23:46:31 +0530

    Update HashMapDemo.java

--- JAVA/Collection/HashMapDemo.java
+++ JAVA/Collection/HashMapDemo.java
@@ -1,3 +1,8 @@
+//Create a HashMap capable of storing String objects. The program should have the following abilities:
+//a) Check if a particular key exists or not.
+//b) Check if a particular value exists or not.
+//c) Use an Iterator to loop through the map.
+
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
